06e827e4afa25ab795e7c9ecb7b3c7a483c92774
Kai Liu <Kai.Liu@thomsonreuters.com>
2019-08-01 17:35:19 +0800

    git init

---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+
+
+## Design Patterns Practice
+
+
+
+
---
+++ src/main/java/com/kay/designpatterns/DesignpatternsApplication.java
@@ -0,0 +1,10 @@
+package com.kay.designpatterns;
+
+//@SpringBootApplication
+public class DesignpatternsApplication {
+
+    public static void main(String[] args) {
+//        SpringApplication.run(DesignpatternsApplication.class, args);
+    }
+
+}
