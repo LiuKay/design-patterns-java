4aa9d249e189e8832306374a6e2fa604a0bcae26
Kai Liu <Kai.Liu@thomsonreuters.com>
2019-08-07 17:32:38 +0800

    Jdk Observer Patterns Disadvantage

---
+++ src/main/java/com/kay/designpatterns/observer/jdk/ForecastDisplay.java
@@ -0,0 +1,37 @@
+package com.kay.designpatterns.observer.jdk;
+
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * Created by Kai.L on 8/7/2019
+ */
+public class ForecastDisplay implements Observer {
+
+    private float currentPressure = 29.92f;
+
+    private float lastPressure;
+
+    private Observable observable;
+
+    public ForecastDisplay(Observable observable) {
+        this.observable = observable;
+        observable.addObserver(this);
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        if (o instanceof WeatherData) {
+            WeatherData weatherData = (WeatherData) o;
+            lastPressure = currentPressure;
+            currentPressure =weatherData.getPressure();
+            display();
+        }
+    }
+
+    private void display() {
+        System.out.println("lastPressure:" + lastPressure + ", currentPressure:" + currentPressure);
+    }
+
+
+}
--- src/main/java/com/kay/designpatterns/observer/jdk/Main.java
+++ src/main/java/com/kay/designpatterns/observer/jdk/Main.java
@@ -1,6 +1,9 @@
 package com.kay.designpatterns.observer.jdk;
 
 /**
+ * JDK 中的 观察者模式违背了 面向对象设计原则：
+ * 1. 没有面对接口编程，Observable 是一个 Class，
+ * 2. setChanged 方法为 protected 只能用子类方式覆盖，不能用组合代替继承的方式扩展
  * Created by Kai.L on 8/6/2019
  */
 public class Main {
@@ -9,7 +12,7 @@ public class Main {
         WeatherData weatherData = new WeatherData();
 
         CurrentConditionDisplay display = new CurrentConditionDisplay(weatherData);
-
+        ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
         weatherData.setStates(22.0f, 40.0f, 120f);
     }
 }
