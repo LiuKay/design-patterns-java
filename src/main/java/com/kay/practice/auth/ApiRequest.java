3cf35c4224f7375d94e6278d96ed0b9d8580add1
LiuKay <kayfen@foxmail.com>
2020-12-26 10:41:28 +0800

    clean dependency

--- src/main/java/com/kay/practice/auth/ApiRequest.java
+++ src/main/java/com/kay/practice/auth/ApiRequest.java
@@ -1,18 +1,19 @@
 package com.kay.practice.auth;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.HashMap;
 import java.util.Map;
-import org.apache.commons.lang3.StringUtils;
 
 public class ApiRequest {
     public static final String TOKEN_PARAM = "token";
     public static final String APP_ID_PARAM = "appId";
     public static final String TIMESTAMP_PARAM = "timestamp";
 
-    private String baseUrl;
-    private String token;
-    private String appId;
-    private long timestamp;
+    private final String baseUrl;
+    private final String token;
+    private final String appId;
+    private final long timestamp;
 
     public ApiRequest(String baseUrl, String token, String appId, long timestamp) {
         this.baseUrl = baseUrl;
@@ -21,20 +22,21 @@ public class ApiRequest {
         this.timestamp = timestamp;
     }
 
+    /**
+     * // if url is encoded, then //        String decode; //        try { //            decode = URLDecoder.decode(url,
+     * "utf-8"); //        } catch (UnsupportedEncodingException e) { //            throw new
+     * RuntimeException(e.getMessage(), e); //        }
+     *
+     * @param url
+     * @return
+     */
     public static ApiRequest createFromFullUrl(String url) {
-        // if url is encoded, then
-//        String decode;
-//        try {
-//            decode = URLDecoder.decode(url, "utf-8");
-//        } catch (UnsupportedEncodingException e) {
-//            throw new RuntimeException(e.getMessage(), e);
-//        }
         String baseUrl = StringUtils.substringBefore(url, "?");
         String params = StringUtils.substringAfter(url, "?");
         String[] split = params.split("&");
         Map<String, String> map = new HashMap<>();
         for (String param : split) {
-            String[] keyValue = param.split("=",2);
+            String[] keyValue = param.split("=", 2);
             map.put(keyValue[0], keyValue[1]);
         }
 
